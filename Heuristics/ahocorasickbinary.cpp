4984a29ced39a0c8e47f8dcdc5fea039df9506d2
Andrew Pitonyak <andrew@pitonyak.org>
2022-09-07 20:10:12 -0400

    Actually add the Aho Corasick search method for binary data.

---
+++ Heuristics/ahocorasickbinary.cpp
@@ -0,0 +1,272 @@
+
+
+#include "ahocorasickbinary.h"
+
+#include <cstring>
+#include <iostream>
+#include <queue>
+
+// This code is adapted from
+// https://gist.github.com/ashpriom/b8231c806edeef50afe1
+// No copyright or license is stated.
+
+
+AhoCorasickBinary::AhoCorasickBinary() : m_num_words(0), m_alphabet_size(ALPHABET_SIZE), m_max_states(0), 
+										 m_out_state(nullptr), m_failure(nullptr), m_goto(nullptr) {
+}
+
+AhoCorasickBinary::~AhoCorasickBinary() {
+	// Wasted cycltes setting the pointers back to null, 
+	// but sometimes, it helps when there are other bugs. 
+	if (m_out_state != nullptr) {
+		delete[] m_out_state;
+		m_out_state = nullptr;
+	}
+	if (m_failure != nullptr) {
+		delete[] m_failure;
+		m_failure = nullptr;
+	}
+	if (m_goto != nullptr) {
+		delete[] m_goto;
+		m_goto = nullptr;
+	}
+}
+
+AhoCorasickBinary::AhoCorasickBinary(const AhoCorasickBinary& x) {
+	m_num_words = x.m_num_words;
+	m_alphabet_size = x.m_alphabet_size;
+	m_max_states = x.m_max_states;
+	if (m_out_state != nullptr) {
+		delete[] m_out_state;
+		m_out_state = nullptr;
+	}
+	if (m_failure != nullptr) {
+		delete[] m_failure;
+		m_failure = nullptr;
+	}
+	if (m_goto != nullptr) {
+		delete[] m_goto;
+		m_goto = nullptr;
+	}
+	if (x.m_out_state != nullptr) {
+		m_out_state = new std::bitset<ALPHABET_SIZE>[m_max_states];
+		m_failure = new int[m_max_states];
+		m_goto = new int[m_alphabet_size * m_max_states];
+
+		for (int i = 0; i<m_alphabet_size * m_max_states; ++i) {
+			m_goto[i] = x.m_goto[i];
+		}
+
+		for (int i=0; i<m_max_states; ++i) {
+			m_failure[i] = x.m_failure[i];
+		}
+
+		for (int i=0; i<m_max_states; ++i) {
+			m_out_state[i] = x.m_out_state[i];
+		}
+	}
+}
+
+const AhoCorasickBinary& AhoCorasickBinary::operator=(const AhoCorasickBinary& x) {
+	if (this != &x) {
+		m_num_words = x.m_num_words;
+		m_alphabet_size = x.m_alphabet_size;
+		m_max_states = x.m_max_states;
+
+		if (m_out_state != nullptr) {
+			delete[] m_out_state;
+			m_out_state = nullptr;
+		}
+		if (m_failure != nullptr) {
+			delete[] m_failure;
+			m_failure = nullptr;
+		}
+		if (m_goto != nullptr) {
+			delete[] m_goto;
+			m_goto = nullptr;
+		}
+		if (x.m_out_state != nullptr) {
+			m_out_state = new std::bitset<ALPHABET_SIZE>[m_max_states];
+			m_failure = new int[m_max_states];
+			m_goto = new int[m_alphabet_size * m_max_states];
+
+			for (int i = 0; i<m_alphabet_size * m_max_states; ++i) {
+				m_goto[i] = x.m_goto[i];
+			}
+
+			for (int i=0; i<m_max_states; ++i) {
+				m_failure[i] = x.m_failure[i];
+			}
+
+			for (int i=0; i<m_max_states; ++i) {
+				m_out_state[i] = x.m_out_state[i];
+			}
+		}
+	}
+	return *this;
+}
+
+int AhoCorasickBinary::buildMatchingMachine(const std::vector<const uint8_t*> &words, const std::vector<int> &word_lengths) {
+	if (m_out_state != nullptr) {
+		delete[] m_out_state;
+		m_out_state = nullptr;
+	}
+	if (m_failure != nullptr) {
+		delete[] m_failure;
+		m_failure = nullptr;
+	}
+	if (m_goto != nullptr) {
+		delete[] m_goto;
+		m_goto = nullptr;
+	}
+	m_max_states = 0;
+	m_num_words = words.size();
+	for (auto const &len: word_lengths) {
+		m_max_states += len;
+	}
+	if (m_num_words == 0 || m_max_states == 0) {
+		return m_max_states;
+	}
+
+	m_out_state = new std::bitset<ALPHABET_SIZE>[m_max_states];
+	m_failure = new int[m_max_states];
+	int goto_size = m_alphabet_size * m_max_states;
+
+	// m_goto[m_max_states][m_alphabet_size]
+	m_goto = new int[goto_size];
+	for (int i = 0; i<goto_size; ++i) {
+		m_goto[i] = -1;
+	}
+
+	for (int i=0; i<m_max_states; ++i) {
+		m_failure[i] = -1;
+	}
+
+	int states = 1; // Initially, we just have the 0 state
+
+	for (int idx_words = 0; idx_words < words.size(); ++idx_words) {
+		const uint8_t* keyword = words[idx_words];
+        int a_word_size = word_lengths[idx_words];
+
+        // Think of current state as how deep into the tree.
+        int currentState = 0;
+        for (int letter_idx = 0; letter_idx < a_word_size; ++letter_idx) {
+            // If there was a "lower bound", it would be keyword[letter_idx] - lower_bound;
+            int idx = getGotoIndex(currentState, keyword[letter_idx]);
+            //std::cout << "Word: " << idx_words << " Letter:" << letter_idx << " is " << keyword[letter_idx] << " currentState = " << currentState << " idx = " << idx << std::endl;
+            if (m_goto[idx] == -1) { // Allocate a new node
+                m_goto[idx] = states++;
+                //std::cout << " allocated node at level " << currentState << " char " << (int) keyword[letter_idx] << " idx:" << idx << std::endl;
+            }
+            currentState = m_goto[idx];
+        }
+        m_out_state[currentState].set(idx_words, true); // There's a match of keywords[idx_words] at node currentState.
+    }
+
+    // State 0 should have an outgoing edge for all characters.
+    for (int i = 0; i < m_alphabet_size; ++i) {
+    	int idx = getGotoIndex(0, i);
+    	if (m_goto[idx] == -1) {
+                m_goto[idx] = 0;
+        }
+    }
+
+    // Build the failure function
+    std::queue<int> q;
+    // Iterate over every possible input
+    for (int c = 0; c < m_alphabet_size; ++c) {
+        // All nodes s of depth 1 have m_failure[s] = 0
+        int idx = getGotoIndex(0, c);
+        if (m_goto[idx] != -1 && m_goto[idx] != 0) {
+            m_failure[m_goto[idx]] = 0;
+            q.push(m_goto[idx]);
+        }
+    }
+
+    while (q.size()) {
+        int state = q.front();
+        q.pop();
+        for (int c = 0; c < m_alphabet_size; ++c) {
+        	int idx = getGotoIndex(state, c);
+            if (m_goto[idx] != -1) {
+                int failure = m_failure[state];
+                while (m_goto[getGotoIndex(failure, c)] == -1) {
+                    failure = m_failure[failure];
+                }
+                failure = m_goto[getGotoIndex(failure, c)];
+                m_failure[m_goto[idx]] = failure;
+                m_out_state[m_goto[idx]] |= m_out_state[failure]; // Merge out values
+                q.push(m_goto[idx]);
+            }
+        }
+    }
+
+    return states;
+}
+
+int AhoCorasickBinary::findNextState(int currentState, uint8_t nextInput) const {
+    int answer = currentState;
+    while (m_goto[getGotoIndex(answer, nextInput)] == -1) {
+    	answer = m_failure[answer];
+    }
+    return m_goto[getGotoIndex(answer, nextInput)];
+}
+
+int AhoCorasickBinary::findFirstMatch(const uint8_t *data, uint32_t len) const {
+	if (data == nullptr) {
+		return -1;
+	}
+	if (m_out_state == nullptr) {
+		std::cerr << "Aho Corasick algorithm has not been initialized." << std::endl;
+		return -1;
+	}
+
+	int currentState = 0;
+    for (int i = 0; i < len; ++i) {
+        currentState = findNextState(currentState, data[i]);
+        if (m_out_state[currentState].none()) continue; // Nothing new, let's move on to the next character.
+        for (int idx_words = 0; idx_words < m_num_words; ++idx_words) {
+           	if (m_out_state[currentState].test(idx_words)) {
+           		return idx_words;
+            }
+       }
+   	}
+
+	return -1;
+}
+
+std::map<int, std::set<int> > AhoCorasickBinary::findAllMatches(const uint8_t *data, uint32_t len) const {
+
+	std::map<int, std::set<int> > matches;
+	std::map<int, std::set<int> >::iterator it;
+
+	if (data == nullptr) {
+		return matches;
+	}
+	if (m_out_state == nullptr) {
+		std::cerr << "Aho Corasick algorithm has not been initialized." << std::endl;
+		return matches;
+	}
+
+	int currentState = 0;
+    for (int i = 0; i < len; ++i) {
+        currentState = findNextState(currentState, data[i]);
+        if (m_out_state[currentState].none()) continue; // Nothing new, let's move on to the next character.
+        for (int idx_words = 0; idx_words < m_num_words; ++idx_words) {
+           	if (m_out_state[currentState].test(idx_words)) {
+           		// Match from: i - keywords[idx_words].size() + 1 to i
+	            //std::cout << "Keyword " << idx_words << " ends at location " << i << std::endl;
+	            it = matches.find(idx_words);
+	            if (it == matches.end()) {
+	            	std::set<int> aSet;
+	            	aSet.insert(i);
+	            	matches.insert( std::make_pair(idx_words, aSet) );
+	            } else {
+	           		it->second.insert(i);
+	            }
+            }
+       }
+   	}
+
+	return matches;
+}
---
+++ Heuristics/ahocorasickbinary.h
@@ -0,0 +1,139 @@
+#ifndef AHO_CORASICK_BINARY_H
+#define AHO_CORASICK_BINARY_H
+
+#include <bitset>
+#include <cstdint>
+#include <map>
+#include <set>
+#include <vector>
+
+
+/**
+
+The Aho Corasick algorithm was developed to search for text / strings. 
+The algorithm allows for multiple strings to be efficiently searched. 
+
+A primary consideration is whether or not to have an array of nodes or if I should
+implement an actual tree using pointers. 
+
+Let K be the number of words to find.
+
+Let n be the length of the text that you search; as in the text that may contain the words you want to find.
+
+Let m be the sum of the length of all of the words that you want to find. So m is the sum of the length of each word you want to find.
+
+The Aho-Corasick algorithm finds all words in O(n+m+z) where z is the total number of occurrences of words in the text. 
+
+First the algorithm builds a Keyword Tree (Trie) that stores the words to find in a tree structure.
+
+
+
+
+The input parameters are defined as: 
+
+
+Given an input text and an array of k words, arr[], find all occurrences of all words in the input text. Let n be the length of text and m be the total number characters in all words, i.e. m = length(arr[0]) + length(arr[1]) + … + length(arr[k-1]). Here k is total numbers of input words.
+
+https://www.geeksforgeeks.org/aho-corasick-algorithm-pattern-searching/
+
+https://gist.github.com/ashpriom/b8231c806edeef50afe1
+
+**/
+
+#define ALPHABET_SIZE 256
+
+class AhoCorasickBinary {
+public:
+    /*! Constructor */
+    AhoCorasickBinary();
+
+    /*! Destructor, clears all structures, not virtual because assume not subclassed. */
+    ~AhoCorasickBinary();
+
+    /*! Constructor */
+    AhoCorasickBinary(const AhoCorasickBinary& x);
+
+    /*! Assignment operator */
+    const AhoCorasickBinary& operator=(const AhoCorasickBinary& x);
+
+    //**************************************************************************
+    //! Build the matching machine.
+    /*!
+     * \param [in]  words Vector of keywords. The index of each keyword is important.
+     * 
+     * \param [in]  word_lengths Vector of work lengths for the keywords.
+     * 
+     * \returns Number of states that the machine has. This is 255 because there are 0 - 255 possible values (searching bytes.)
+     *
+     ***************************************************************************/
+    int buildMatchingMachine(const std::vector<const uint8_t *> &words, const std::vector<int> &word_lengths);
+
+    //**************************************************************************
+    //! Find the first match (no order). Use to see if there is at least one match.
+    /*!
+     * \param [in]  data Pointer to the data. Will search this data to see if it contains any keyword.
+     * 
+     * \param [in]  len Length of the data to search; think number of characters.
+     * 
+     * \returns An arbitray match or -1 if no match is found.
+     *
+     ***************************************************************************/
+    int findFirstMatch(const uint8_t *data, uint32_t len) const;
+
+    //**************************************************************************
+    //! All matches. The key to the map indicates the index of the keyword. The set contains the ending location of the match.
+    /*!
+     * \param [in]  data Pointer to the data. Will search this data to see if it contains any keyword.
+     * 
+     * \param [in]  len Length of the data to search; think number of characters.
+     * 
+     * \returns All matches with the keyword index as the map key and the ending location in data as the set.
+     *
+     ***************************************************************************/
+    std::map<int, std::set<int> > findAllMatches(const uint8_t *data, uint32_t len) const;
+
+    int getNumWords() const { return m_num_words; }
+    int getAlphabetSize() const { return m_alphabet_size; }
+    int getMaxStates() const { return m_max_states; }
+
+private:
+    //**************************************************************************
+    //! Find the next state for a transition.
+    /*!
+     * \param [in]  currentState - The current state of the machine. Must be between 0 and the number of states - 1, inclusive (so 0 and 255).
+     * 
+     * \param [in]  nextInput - The next character that enters into the machine. 
+     * 
+     * \returns Number of states that the machine has. This is 255 because there are 0 - 255 possible values (searching bytes.)
+     *
+     ***************************************************************************/
+    int findNextState(int currentState, uint8_t nextInput) const;
+
+	int getGotoIndex(const int state_idx, const int value_idx) const;
+
+	/*! Number of words. */
+	int m_num_words;
+
+	/*! Number of valid values. If searching characters, might be 26, one for each leter. This code searches 8-bit bytes so 256 possibilities. */
+	int m_alphabet_size;
+
+    /*! Maximum number of states. This consists of the sum of the length of all "words" that will be searched. In other words, maximum number of nodes. */
+	int m_max_states;
+
+	/*! Array of size m_max_states. Output for each state as a mask if the keyword with index i appears when the machine enters this state. */
+	std::bitset<ALPHABET_SIZE>* m_out_state;
+
+	/*! Array of size m_max_states. This is the failure function used internally. */
+	int* m_failure;
+
+	/*! Array of size [m_max_states][m_alphabet_size]. This is the "Goto" function or -1 if fail. */
+	int* m_goto;
+};
+
+inline int AhoCorasickBinary::getGotoIndex(const int state_idx, const int value_idx) const {
+	// Add error checking if needed / desired.
+	return state_idx * m_alphabet_size + value_idx;
+}
+
+
+#endif
